973627bb4233f500b554f5d076ceb7c55af4d801
Le Anh Vu <31981595+zz2zz22@users.noreply.github.com>
2021-06-30 13:37:45 +0700

    Configure Auth system

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {Component} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 import * as firebase from 'firebase'
@@ -24,15 +24,58 @@ import { createStackNavigator } from '@react-navigation/stack';
 import LandingScreen from './components/auth/Landing'
 import RegisterScreen from './components/auth/Register'
 
-
 const Stack = createStackNavigator();
-export default function App() {
-  return (
-    <NavigationContainer>
+
+export class App extends Component {
+constructor(props){
+  super(props);
+
+  this.state = {
+    loaded: false,
+  }
+}
+
+componentDidMount(){
+  firebase.default.auth().onAuthStateChanged((user) => {
+    if(!user){
+      this.setState({
+        loggedIn: false,
+        loaded: true,
+      })
+    }else{
+      this.setState({
+        loggedIn: true,
+        loaded: true,
+      })
+    }
+  })
+}
+render() {
+  const {loggedIn, loaded} = this.state
+  if(!loaded){
+    return(
+      <View style={{flex: 1, justifyContent: 'center'}}>
+        <Text>Loading</Text>
+      </View>
+    )
+  }
+
+  if(!loggedIn){
+    return (
+      <NavigationContainer>
       <Stack.Navigator initialRouteName="Landing">
         <Stack.Screen name="Landing" component={LandingScreen} options={{ headerShown: false }}/>
         <Stack.Screen name="Register" component={RegisterScreen}/>
       </Stack.Navigator>
     </NavigationContainer>
-  );
+    );
+  }   
+  return(
+    <View style={{flex: 1, justifyContent: 'center'}}>
+      <Text>User is logged in</Text>
+    </View>
+  )
 }
+}
+
+export default App
