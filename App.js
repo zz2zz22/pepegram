4c598b2428dbd6e5af659f98000c7c7ab5b636e8
Le Anh Vu <31981595+zz2zz22@users.noreply.github.com>
2021-06-30 13:11:59 +0700

    Register screen + Firebase Initialize

--- App.js
+++ App.js
@@ -2,20 +2,37 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import * as firebase from 'firebase'
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBVBLdVqWG_OkE2xk9kOoqfRO_-Fe3xGX0",
+  authDomain: "pepegram-5488f.firebaseapp.com",
+  projectId: "pepegram-5488f",
+  storageBucket: "pepegram-5488f.appspot.com",
+  messagingSenderId: "875650020500",
+  appId: "1:875650020500:web:e8f2fa21bda60ae8b3b081",
+  measurementId: "G-JZY3EJL8NB"
+};
+
+if(firebase.apps.length == 0){
+  firebase.initializeApp(firebaseConfig)
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import LandingScreen from './components/auth/Landing'
+import RegisterScreen from './components/auth/Register'
+
+
+const Stack = createStackNavigator();
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Landing">
+        <Stack.Screen name="Landing" component={LandingScreen} options={{ headerShown: false }}/>
+        <Stack.Screen name="Register" component={RegisterScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
