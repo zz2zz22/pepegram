70bf663d16c600fb2b809c0e7786fa2f060dffb3
Le Anh Vu <31981595+zz2zz22@users.noreply.github.com>
2021-07-01 10:24:00 +0700

    Customize bottom tab navagator

--- components/Main.js
+++ components/Main.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 
 import {connect} from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { fetchUser } from '../redux/actions/index'
 
 import FeedScreen from './main/Feed'
+import AddScreen from './main/Add'
+import ProfileScreen from './main/Profile'
 
 const Tab = createBottomTabNavigator();
 
@@ -17,7 +20,24 @@ export class Main extends Component {
     render() {
         return (
             <Tab.Navigator>
-                <Tab.Screen name="Feed" component={FeedScreen}/>
+                <Tab.Screen name="Feed" component={FeedScreen}
+                options={{
+                    tabBarIcon: ({color, size}) => (
+                        <MaterialCommunityIcons name="home" color={color} size={26}/>
+                    )
+                }}/>
+                <Tab.Screen name="Add" component={AddScreen}
+                options={{
+                    tabBarIcon: ({color, size}) => (
+                        <MaterialCommunityIcons name="plus-box" color={color} size={26}/>
+                    )
+                }}/>
+                <Tab.Screen name="Profile" component={ProfileScreen}
+                options={{
+                    tabBarIcon: ({color, size}) => (
+                        <MaterialCommunityIcons name="account-circle" color={color} size={26}/>
+                    )
+                }}/>
             </Tab.Navigator>
         )
     }
