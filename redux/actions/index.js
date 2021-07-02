1791cb775c0d0211d2d91679b939b445d18410a4
Le Anh Vu <31981595+zz2zz22@users.noreply.github.com>
2021-07-02 13:43:10 +0700

    Fetching Posts from Firestore database

--- redux/actions/index.js
+++ redux/actions/index.js
@@ -1,4 +1,4 @@
-import { USER_STATE_CHANGE } from '../constants/index'
+import { USER_STATE_CHANGE, USER_POSTS_STATE_CHANGE } from '../constants/index'
 import firebase from "firebase";
 
 export function fetchUser(){
@@ -14,6 +14,26 @@ export function fetchUser(){
             else{
                 console.log('does not exist')
             }
+        })
+    })
+}
+
+export function fetchUserPosts(){
+    return((dispatch) => {
+        firebase.firestore()
+        .collection("posts")
+        .doc(firebase.auth().currentUser.uid)
+        .collection("userPosts")
+        .orderBy("creation", "asc")
+        .get()
+        .then((snapshot) => {
+            let posts = snapshot.docs.map(doc => {
+                const data = doc.data();
+                const id = doc.id;
+                return{id, ...data}
+            })
+            console.log(posts)
+            dispatch({type: USER_POSTS_STATE_CHANGE, posts})
         })
     })
 }
