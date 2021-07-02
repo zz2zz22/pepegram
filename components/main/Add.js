a8969ec64a46e63a9d7c944dbf9bacc7116e2031
Le Anh Vu <31981595+zz2zz22@users.noreply.github.com>
2021-07-02 08:50:00 +0700

    Update the take picture function
    - Show the picture below the camera to test the function

--- components/main/Add.js
+++ components/main/Add.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet,Text, View, Button } from 'react-native';
+import { StyleSheet,Text, View, Button, Image } from 'react-native';
 import { Camera } from 'expo-camera';
 
 export default function App() {
   const [hasPermission, setHasPermission] = useState(null);
+  const [camera, setCamera] = useState(null);
+  const [image, setImage] = useState(null);
   const [type, setType] = useState(Camera.Constants.Type.back);
 
   useEffect(() => {
@@ -12,6 +14,13 @@ export default function App() {
       setHasPermission(status === 'granted');
     })();
   }, []);
+
+  const takePicture = async () => {
+      if(camera){
+          const data = await camera.takePictureAsync(null)
+          setImage(data.uri)
+      }
+  }
 
   if (hasPermission === null) {
     return <View />;
@@ -23,6 +32,7 @@ export default function App() {
     <View style={{flex: 1}}>
         <View style={styles.cameraContainer}>
          <Camera 
+            ref={ ref => setCamera(ref)}
             style={styles.fixedRatio} 
             type={type}
             ratio={'1:1'}/>
@@ -37,6 +47,8 @@ export default function App() {
               );
             }}>
           </Button>
+          <Button title="Take Picture" onPress={() => takePicture()}/>
+          {image && <Image source={{uri: image}} style={{flex:1}}/>}
     </View>
   );
 }
